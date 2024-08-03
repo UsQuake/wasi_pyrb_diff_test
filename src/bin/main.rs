902d14b7d4db459e50795090e126242ac9a0839e
UsQuake <juju020716@gmail.com>
2024-08-04 02:41:15 +0900

    Add Ctrl-c end feature

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,86 +1,45 @@
-use wasi_pyrb_diff_test::grammar::{str_helper::replace_scope_with_indent,predef_grammars::get_python_grammar, Union};
-use wasi_pyrb_diff_test::grammar_fuzzer::var_ctx::ir_to_ctx;
-use wasi_pyrb_diff_test::grammar_fuzzer::GrammarsFuzzer;
-use wasi_pyrb_diff_test::testcase_driver::exec::{execute_test, PrintResult, PYTHON_TEST_INFOS};
+use docker_api::{models::ContainerSummary, opts::{ContainerListOpts, ContainerStopOpts}, Container};
+use exec::{exec_test, LanguageType};
+use tokio::signal;
 use wasi_pyrb_diff_test::testcase_driver::*;
 
 
 #[tokio::main]
 async fn main() {
-            let test_count = 10000;
-            let mut cli_progress_bar = IndicateBar::new(test_count);
-            let mut testcase_generator = GrammarsFuzzer::new(
-                &get_python_grammar(),
-                "<start>",
-                10,
-                100,
-                Union::OnlyA(false),
-            );
-            let mut docker = docker_api::Docker::new("unix:///var/run/docker.sock").unwrap();
-            //let mut rand_seed = (SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() & ((1<<65) - 1)) as u64;
-            let mut rand_seed = 17526186317047798642;
-            for i in 0..test_count{ 
-                cli_progress_bar.progress_one();
-                let testcase = testcase_generator.fuzz(&mut rand_seed);
-                let test_input = replace_scope_with_indent(&ir_to_ctx(&testcase, &mut rand_seed.clone()));
-                let mut results:Vec<PrintResult> = Vec::new(); 
+    let test_count = 10000;
+    let mut docker = docker_api::Docker::new("unix:///var/run/docker.sock").unwrap();
+    //let rand_seed = (SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() & ((1<<65) - 1)) as u64;
+    let rand_seed = 17526186317047798642;
+    let test_exec = exec_test(&mut docker, test_count, rand_seed, LanguageType::Python);
+    tokio::select! {
+    _ = test_exec=>{},
+    _ = signal::ctrl_c()  => {
+        println!("Ctrl-C detected. End test. Bye!");
+    let opts = ContainerListOpts::builder()
+                .all(true)
+                .build();
 
-                for test_info in PYTHON_TEST_INFOS{
-                    let result = execute_test(&mut docker, test_info, test_input.as_bytes()).await;
-                    results.push(result);
-                }
-            
-                let is_v8_and_jsc_result_same = results[0].stdout == results[1].stdout;
-                let is_v8_and_spidermonkey_result_same = results[0].stdout == results[2].stdout;
-                let is_jsc_and_spidermonkey_result_same = results[1].stdout == results[2].stdout;
-                
-                let are_js_engines_result_same = (is_v8_and_jsc_result_same && is_v8_and_spidermonkey_result_same) && is_jsc_and_spidermonkey_result_same;
-
-                let is_v8_and_native_result_same = results[0].stdout == results[3].stdout.clone() + &results[3].stderr;
-                let is_jsc_and_native_result_same = results[1].stdout == results[3].stdout.clone() + &results[3].stderr;
-                let is_spidermonkey_and_native_result_same = results[2].stdout == results[3].stdout.clone() + &results[3].stderr;
-                
-                let is_native_wasm_same = (is_v8_and_native_result_same && is_jsc_and_native_result_same) && is_spidermonkey_and_native_result_same;
-               
-
-                if !are_js_engines_result_same{
-
-                    if is_jsc_and_spidermonkey_result_same{
-
-                        println!("⚠Warning!: V8 result is different with other engines!");
-                        std::fs::write("./issues/v8/log".to_string() + &i.to_string() + ".txt", &results[0].stdout).unwrap();
-                        std::fs::write("./issues/v8/testcase".to_string() + &i.to_string() + ".py", &test_input).unwrap();
-
-                    }else if is_v8_and_spidermonkey_result_same{
-
-                        println!("⚠Warning!: JavascriptCore result is different with other engines!");
-                        std::fs::write("./issues/jsc/log".to_string() + &i.to_string() + ".txt", &results[1].stdout).unwrap();
-                        std::fs::write("./issues/jsc/testcase".to_string() + &i.to_string() + ".py", &test_input).unwrap();
-
-                    }else if is_v8_and_jsc_result_same{
-
-                        println!("⚠Warning!: SpiderMonkey result is different with other engines!");
-                        std::fs::write("./issues/spm/log".to_string() + &i.to_string() + ".txt", &results[2].stdout).unwrap();
-                        std::fs::write("./issues/spm/testcase".to_string() + &i.to_string() + ".py", &test_input).unwrap();
-
-                    }else{
-
-                        println!("⚠Warning!: Each result of js engines different with other engines!");
-                        std::fs::write("./issues/unknown/log".to_string() + &i.to_string() + ".txt", 
-                        format!("d8(V8):\n{}\n", results[0].stdout)
-                        + &format!("jsc(JavascriptCore):\n{}\n", results[1].stdout)
-                        + &format!("JsShell(SpiderMonkey):\n{}\n", results[2].stdout)).unwrap();
-                        std::fs::write("./issues/unknown/testcase".to_string() + &i.to_string() + ".py", &test_input).unwrap();
-
-                    }
-
-                }else if !is_native_wasm_same{
-                    std::fs::write("./issues/native_vs_wasm/log".to_string() + &i.to_string() + ".txt", 
-                    format!("d8(V8):\n{}\n", results[0].stdout)
-                    + &format!("Native:\n{}\n", results[3].stdout.clone() + &results[3].stderr)).unwrap();
-                    std::fs::write("./issues/native_vs_wasm/testcase".to_string() + &i.to_string() + ".py", &test_input).unwrap();
-                    //omit_testcase_or_other_name("./issued_testcases/native_vs_wasm", &test_input, &LanguageType::Python);
+    if let Ok(containers) = docker.containers().list(&opts).await {
+        let cont_names:Vec<ContainerSummary> = containers.into_iter()
+        .filter(|container| container.clone().names.map(|n| n[0].to_owned()).unwrap_or_default() == "testcase_execution_container")
+        .collect();
+        if cont_names.len() == 1{
+            let container = docker.containers().get(cont_names[0].id.clone().unwrap());
+            let opts = ContainerStopOpts::builder()
+            .wait(std::time::Duration::from_secs(0))
+            .build();
+            if let Err(e) =  container.stop(&opts).await {
+                    eprintln!("Error: {e}");
+            };
+        
+            if let Err(e) = container.remove(&Default::default()).await {
+                    eprintln!("Error: {e}")
+            }
                 }
             }
+            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        }
 
+    }
 }
+
--- src/testcase_driver/exec.rs
+++ src/testcase_driver/exec.rs
@@ -4,43 +4,15 @@ use futures::StreamExt;
 use tokio::time::{sleep, Duration};
 use std::str;
 
-pub static PYTHON_TEST_INFOS:& 'static [TestInfo;4] =  &[
-    TestInfo{
-    target_platform:PlatformType::V8,
-    target_language:LanguageType::Python
-    },
-     TestInfo{
-    target_platform:PlatformType::JavascriptCore,
-    target_language:LanguageType::Python
-    }, 
-     TestInfo{
-    target_platform:PlatformType::SpiderMonkey,
-    target_language:LanguageType::Python
-    }, 
-    TestInfo{
-    target_platform:PlatformType::Native,
-    target_language:LanguageType::Python
-    }
-];
+use crate::grammar::predef_grammars::{get_python_grammar, get_ruby_grammar};
+use crate::grammar::str_helper::replace_scope_with_indent;
+use crate::grammar::Union;
+use crate::grammar_fuzzer::var_ctx::ir_to_ctx;
+use crate::grammar_fuzzer::GrammarsFuzzer;
 
-pub static RUBY_TEST_INFOS:& 'static [TestInfo;4] =  &[
-    TestInfo{
-    target_platform:PlatformType::V8,
-    target_language:LanguageType::Ruby
-    },
-     TestInfo{
-    target_platform:PlatformType::JavascriptCore,
-    target_language:LanguageType::Ruby
-    }, 
-     TestInfo{
-    target_platform:PlatformType::SpiderMonkey,
-    target_language:LanguageType::Ruby
-    }, 
-    TestInfo{
-    target_platform:PlatformType::Native,
-    target_language:LanguageType::Ruby
-    }
-]; 
+use super::IndicateBar;
+
+
 pub struct PrintResult{
     pub stdout: String,
     pub stderr: String
@@ -55,6 +27,129 @@ pub enum LanguageType{
 pub struct TestInfo{
     pub target_platform:PlatformType,
     pub target_language:LanguageType
+}
+pub async fn exec_test(docker:&mut Docker, test_count: u32, init_seed: u64, object_interpreter:LanguageType){
+    let mut cli_progress_bar = IndicateBar::new(test_count);
+    let language_grammar = match object_interpreter{
+        LanguageType::Python => &get_python_grammar(),
+        LanguageType::Ruby => &get_ruby_grammar()
+    };
+    let testcase_file_ext = match object_interpreter{
+        LanguageType::Python => ".py",
+        LanguageType::Ruby => ".rb"
+    };
+
+    let test_exec_infos = match object_interpreter{
+        LanguageType::Python =>  &[
+            TestInfo{
+            target_platform:PlatformType::V8,
+            target_language:LanguageType::Python
+            },
+             TestInfo{
+            target_platform:PlatformType::JavascriptCore,
+            target_language:LanguageType::Python
+            }, 
+             TestInfo{
+            target_platform:PlatformType::SpiderMonkey,
+            target_language:LanguageType::Python
+            }, 
+            TestInfo{
+            target_platform:PlatformType::Native,
+            target_language:LanguageType::Python
+            }
+        ],
+        LanguageType::Ruby =>  &[
+            TestInfo{
+            target_platform:PlatformType::V8,
+            target_language:LanguageType::Ruby
+            },
+             TestInfo{
+            target_platform:PlatformType::JavascriptCore,
+            target_language:LanguageType::Ruby
+            }, 
+             TestInfo{
+            target_platform:PlatformType::SpiderMonkey,
+            target_language:LanguageType::Ruby
+            }, 
+            TestInfo{
+            target_platform:PlatformType::Native,
+            target_language:LanguageType::Ruby
+            }
+        ]
+    };
+    let mut testcase_generator = GrammarsFuzzer::new(
+        &language_grammar,
+        "<start>",
+        10,
+        100,
+        Union::OnlyA(false),
+    );
+    let mut rand_seed = init_seed.clone();
+    
+    for i in 0..test_count{ 
+        cli_progress_bar.progress_one();
+        let testcase = testcase_generator.fuzz(&mut rand_seed);
+        let test_input = replace_scope_with_indent(&ir_to_ctx(&testcase, &mut rand_seed.clone()));
+        let mut results:Vec<PrintResult> = Vec::new(); 
+
+
+        for test_info in test_exec_infos{
+            let result = execute_test(docker, test_info, test_input.as_bytes()).await;
+            results.push(result);
+        }
+    
+        let is_v8_and_jsc_result_same = results[0].stdout == results[1].stdout;
+        let is_v8_and_spidermonkey_result_same = results[0].stdout == results[2].stdout;
+        let is_jsc_and_spidermonkey_result_same = results[1].stdout == results[2].stdout;
+        
+        let are_js_engines_result_same = (is_v8_and_jsc_result_same && is_v8_and_spidermonkey_result_same) && is_jsc_and_spidermonkey_result_same;
+
+        let is_v8_and_native_result_same = results[0].stdout == results[3].stdout.clone() + &results[3].stderr;
+        let is_jsc_and_native_result_same = results[1].stdout == results[3].stdout.clone() + &results[3].stderr;
+        let is_spidermonkey_and_native_result_same = results[2].stdout == results[3].stdout.clone() + &results[3].stderr;
+        
+        let is_native_wasm_same = (is_v8_and_native_result_same && is_jsc_and_native_result_same) && is_spidermonkey_and_native_result_same;
+       
+
+        if !are_js_engines_result_same{
+
+            if is_jsc_and_spidermonkey_result_same{
+
+                println!("⚠Warning!: V8 result is different with other engines!");
+                std::fs::write("./issues/v8/log".to_string() + &i.to_string() + ".txt", &results[0].stdout).unwrap();
+                std::fs::write("./issues/v8/testcase".to_string() + &i.to_string() + testcase_file_ext, &test_input).unwrap();
+
+            }else if is_v8_and_spidermonkey_result_same{
+
+                println!("⚠Warning!: JavascriptCore result is different with other engines!");
+                std::fs::write("./issues/jsc/log".to_string() + &i.to_string() + ".txt", &results[1].stdout).unwrap();
+                std::fs::write("./issues/jsc/testcase".to_string() + &i.to_string() + testcase_file_ext, &test_input).unwrap();
+
+            }else if is_v8_and_jsc_result_same{
+
+                println!("⚠Warning!: SpiderMonkey result is different with other engines!");
+                std::fs::write("./issues/spm/log".to_string() + &i.to_string() + ".txt", &results[2].stdout).unwrap();
+                std::fs::write("./issues/spm/testcase".to_string() + &i.to_string() + testcase_file_ext, &test_input).unwrap();
+
+            }else{
+
+                println!("⚠Warning!: Each result of js engines different with other engines!");
+                std::fs::write("./issues/unknown/log".to_string() + &i.to_string() + ".txt", 
+                format!("d8(V8):\n{}\n", results[0].stdout)
+                + &format!("jsc(JavascriptCore):\n{}\n", results[1].stdout)
+                + &format!("JsShell(SpiderMonkey):\n{}\n", results[2].stdout)).unwrap();
+                std::fs::write("./issues/unknown/testcase".to_string() + &i.to_string() + testcase_file_ext, &test_input).unwrap();
+
+            }
+
+        }else if !is_native_wasm_same{
+            std::fs::write("./issues/native_vs_wasm/log".to_string() + &i.to_string() + ".txt", 
+            format!("d8(V8):\n{}\n", results[0].stdout)
+            + &format!("Native:\n{}\n", results[3].stdout.clone() + &results[3].stderr)).unwrap();
+            std::fs::write("./issues/native_vs_wasm/testcase".to_string() + &i.to_string() + testcase_file_ext, &test_input).unwrap();
+            //omit_testcase_or_other_name("./issued_testcases/native_vs_wasm", &test_input, &LanguageType::Python);
+        }
+    }
 }
 
 pub async fn execute_test<'a>(docker:&mut Docker, test_target:&TestInfo, testcase: &'a [u8])-> PrintResult{
